b1d2cfdfee58ba33c8c96b5f73f3f1b74a67bcde
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2016-03-23 22:54:51 +0100

    Fix #3

--- lib/src/main/java/com/tobiasrohloff/view/NestedScrollWebView.java
+++ lib/src/main/java/com/tobiasrohloff/view/NestedScrollWebView.java
@@ -39,16 +39,21 @@ public class NestedScrollWebView extends WebView implements NestedScrollingChild
     private NestedScrollingChildHelper mChildHelper;
 
     public NestedScrollWebView(Context context) {
-        this(context, null);
+        super(context);
+        init();
     }
 
     public NestedScrollWebView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+        super(context, attrs);
+        init();
     }
 
     public NestedScrollWebView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
 
+    private void init() {
         mChildHelper = new NestedScrollingChildHelper(this);
         setNestedScrollingEnabled(true);
     }
